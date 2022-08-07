dee324d8fe7c764ea69c78abc96810cd7c4733cc
yujia_yan <1996534+Yujia-Yan@users.noreply.github.com>
2022-08-07 12:28:43 -0400

    add soxr to requirements

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ requirements= [i.strip() for i in open("requirements.txt").readlines()]
 
 setuptools.setup(
     name="transkun",
-    version="0.1.1",
+    version="0.1.2",
     author="Yujia Yan",
     description='Yet another tool for automatic piano transcription',
     long_description=long_description,
